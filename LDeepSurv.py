4e6e514c9154b92109fda81aeb7ca417dc245294
Pei Liu <18200144374@163.com>
2017-12-04 18:16:12 +0800

    add code for compiling Keras model

--- LDeepSurv.py
+++ LDeepSurv.py
@@ -1,8 +1,14 @@
+from __future__ import print_function
+import numpy as np
 import keras
+import theano.tensor as T
+from input_data import read_data_sets
+from lifelines.utils import concordance_index
+import utils
 
 class LDeepSurv(object):
-	def __init__(self, input_shape, learning_rate = 0.001, lr_decay = 0.0, 
-        L2_reg = 0.0, L1_reg = 0.0, optimizer = 'Adam', 
+	def __init__(self, input_shape, learning_rate = 0.001, 
+        L2_reg = 0.0, L1_reg = 0.0, optimizer = 'adam', 
         hidden_layer_sizes = None, activation = 'relu', 
         batch_norm = False, dropout = 0.0, standardize = False):
         """
@@ -10,7 +16,6 @@ class LDeepSurv(object):
         Parameters:
             input_shape: shape of input. like (None, n)
             learning_rate: learning rate for training.
-            lr_decay: coefficient for Power learning rate decay.
             L2_reg: coefficient for L2 weight decay regularization. Used to help
                 prevent the model from overfitting.
             L1_reg: coefficient for L1 weight decay regularization
@@ -25,7 +30,8 @@ class LDeepSurv(object):
             standardize: True or False. Include standardization layer after
                 input layer.
         """
-        X = keras.layers.Input(shape = input_shape, dtype = "float32", name = 'Input')
+        X_input = keras.layers.Input(shape = input_shape, dtype = "float32", name = 'Input')
+        X = X_input
 
         for n_layer in hidden_layer_sizes:
             # weight initialization methods for different activations
@@ -35,35 +41,48 @@ class LDeepSurv(object):
                 # TODO:
                 W_init = 'glorot_uniform'
 
-            # X = w * x + b
+            # Z = w * x + b
             # weight initialize and regularize
-            X = keras.layers.Dense(units = n_layer, 
+            Z = keras.layers.Dense(units = n_layer, 
                                    kernel_initializer = W_init,
                                    kernel_regularize = keras.regularizers.l1_l2(L1_reg, L2_reg))(X)
 
             # batch Norm should be added before Activation
             # the advantages of BatchNorm
             if batch_norm:
-                X = keras.layers.BatchNormalization()(X)
+                Z = keras.layers.BatchNormalization()(Z)
 
             # activation function
-            X = keras.layers.Activation(activation)
+            A = keras.layers.Activation(activation)(Z)
 
             # Dropout
             if dropout != .0:
-                X = keras.layers.Dropout(dropout)(X)
+                A = keras.layers.Dropout(dropout)(A)
+
+            X = A
 
         # output
         output = keras.layer.Dense(units = 1,
-                              kernel_initializer = 'glorot_uniform',
-                              kernel_regularize = keras.regularizers.l1_l2(L1_reg, L2_reg))(X)
+                                   kernel_initializer = 'glorot_uniform',
+                                   kernel_regularize = keras.regularizers.l1_l2(L1_reg, L2_reg))(X)
 
-        self.output = output
+        # model
+        self.model = keras.models.Model(inputs = X_input, outputs = output, name = 'LDeepSurv')
+        
+        # optimizer
+        if optimizer == 'adam':
+            opt = keras.optimizers.Adam()
+        elif optimizer == 'sgd':
+            opt = keras.optimizers.SGD(lr = learning_rate, momentum = 0.9, decay = 0.0, nesterov = False)
+        else:
+            opt = keras.optimizers.Adam()
+
+        self.model.compile(optimizer = opt, loss = self._negative_log_likelihood, metrics = [self._CI])
+
         # Store and set needed Hyper-parameters for tuning
         self.hyperparams = {
             'learning_rate': learning_rate,
             'hidden_layers_sizes': hidden_layers_sizes,
-            'lr_decay': lr_decay,
             'optimizer': optimizer,
             'L2_reg': L2_reg,
             'L1_reg': L1_reg,
@@ -73,13 +92,73 @@ class LDeepSurv(object):
             'standardize': standardize
         }
 
-        # Store needed parameters for trainning or others
+        # Store needed parameters for training or others
         self.input_shape = input_shape
         self.learning_rate = learning_rate
-        self.lr_decay = lr_decay
-        self.optimizer = optimizer
         self.standardize = standardize
         self.restored_update_params = None
 
-    def loss_function(self):
-        
+    def _negative_log_likelihood(self, y_true, y_pred):
+        """
+        Callable loss function for DeepSurv network.
+
+        Parameters:
+            y_true: labels(dict like {'E': ,'T': }) for input_X.
+            y_pred: outputs of the network for input_X.
+
+        Returns:
+            loss value formatted as theano expression.
+        """
+        # get information from labels for next calculation.
+        (ties, E, T, failures, atrisk) = utils.decode_ET(y_true)
+        logL = 0
+        if ties == 'noties':
+            hazard_ratio = T.exp(y_pred)
+            log_risk = T.log(T.extra_ops.cumsum(hazard_ratio))
+            likelihood = y_pred.T - log_risk
+            uncensored_likelihood = likelihood * E
+            logL = -T.sum(uncensored_likelihood)
+        else:
+            for t in failures:
+                tfail = failures[t]
+                trisk = atrisk[t]
+                d = len(tfail)
+
+                logL += -T.sum(y_pred[tfail])
+
+                if ties == 'breslow':
+                    s = T.sum(T.exp(y_pred[trisk]))
+                    logL += T.log(s)*d
+                elif ties == 'efron':
+                    s = T.sum(T.exp(y_pred[trisk]))
+                    r = T.sum(T.exp(y_pred[tfail]))
+                    for j in range(d):
+                        logL += T.log(s - j * r / d)
+                else:
+                    raise NotImplementedError('tie breaking method not recognized')
+
+        return logL
+    
+    def _CI(self, y_true, y_pred):
+        """
+        Compute the concordance-index value.
+        Parameters:
+            y_true: labels(dict like {'e': ,'t': }) for input_X.
+            y_pred: outputs of the network for input_X.
+
+        Returns:
+            concordance index.
+        """
+        hr_pred = -np.exp(y_pred)
+        ci = concordance_index(y_true['t'], hr_pred, y_true['e'])
+        return ci
+
+    def train(self, data_dir, vr = 0.2, batch_size = 1000, epoch = 10000):
+
+        # DataSets = read_data_sets(train_dir = data_dir, validation_ratio = vr)
+        # train_data, validation_data, test_data = DataSets.train, DataSets.validation, DataSets.test
+
+        # for num_iters in range(epoch):
+
+        #     for i in range(train_data.num_examples / batch_size):
+        #         batch_data = train_data.next_batch(batch_size = batch_size)
---
+++ utils.py
@@ -0,0 +1,134 @@
+import numpy as np
+import random
+import pandas as pd
+
+def prepare_data(data):
+    if isinstance(data, dict):
+       x, e, t = data['x'], data['e'], data['t']
+
+    # Sort Training Data for Accurate Likelihood
+    sort_idx = np.argsort(t)[::-1]
+    x = x[sort_idx]
+    e = e[sort_idx]
+    t = t[sort_idx]
+
+    print 'Censored data= ', 1- np.mean(e), '%'
+
+    return x, e, t
+
+def decode_ET(y_data):
+    if isinstance(y_data, dict):
+       e, t = data_y['e'], data_y['t']
+
+    failures = {}
+    atrisk = {}
+
+    n, cnt = 0, 0
+    for i in range(e.shape[0]):
+        if e[i]:
+            if t[i] not in failures:
+                failures[t[i]] = [i]
+                n += 1
+            else:
+                # ties occured
+                cnt += 1
+                failures[t[i]].append(i)
+
+            if t[i] not in atrisk:
+                atrisk[t[i]] = [i]
+                for j in range(0, i):
+                    atrisk[t[i]].append(i-j-1)
+            else:
+                atrisk[t[i]].append(i)
+
+    # when ties occured frequently
+    if cnt >= n / 2:
+        ties = 'efron'
+    elif cnt > 0:
+        ties = 'breslow'
+    else:
+        ties = 'noties'
+
+    return ties, e, t, failures, atrisk
+
+def readData(file0, file1, discount):
+    random.seed(1)
+    # read file
+    data0 = pd.read_csv(file0)
+    data1 = pd.read_csv(file1)
+    names = np.array(list(data0))
+    if data0.isnull().any().any():
+        data0 = data0.fillna(0)
+    if data1.isnull().any().any():
+        data1 = data1.fillna(0)
+
+    TRAIN_NUM0 = data0.shape[0]
+    TRAIN_NUM1 = data1.shape[0]
+    FEATURE_NUM = data0.shape[1]-2
+
+    # randomly split data0 and data1 to train and test
+    cnt0 = range(TRAIN_NUM0)
+    random.shuffle(cnt0)
+    trainidx0 = cnt0[0:int(TRAIN_NUM0 * discount)]
+    testidx0 = cnt0[int(TRAIN_NUM0 * discount) + 1:TRAIN_NUM0]
+
+    cnt1 = range(TRAIN_NUM1)
+    random.shuffle(cnt1)
+    trainidx1 = cnt1[0:int(TRAIN_NUM1 * discount)]
+    testidx1 = cnt1[int(TRAIN_NUM1 * discount) + 1:TRAIN_NUM1]
+
+    # generate train data0
+    x0 = data0.values[trainidx0, 0:FEATURE_NUM]
+    t0 = data0.values[trainidx0, FEATURE_NUM]
+    e0 = data0.values[trainidx0, FEATURE_NUM+1]
+
+    # generate train data1
+    x1 = data1.values[trainidx1, 0:FEATURE_NUM]
+    t1 = data1.values[trainidx1, FEATURE_NUM]
+    e1 = data1.values[trainidx1, FEATURE_NUM+1]
+
+    x = np.concatenate((x0, x1), axis=0)
+    t = np.concatenate((t0, t1), axis=0)
+    e = np.concatenate((e0, e1), axis=0)
+    #normlize
+    df = pd.DataFrame(x)
+    df = df.replace(-1, 9)
+    ndf = (df - df.min()) / (df.max() - df.min())
+    x = ndf.values
+    train_data = {
+        'x': x.astype(np.float32),
+        't': t.astype(np.int8),
+        'e': e.astype(np.int8)
+    }
+
+    #train_data['x'] -= np.mean(train_data['x'], axis=0)
+    print 'train t=1: min-', min(t1), ' max-', max(t1), ' mean-', np.mean(t1)
+
+    # generate test data
+    x0 = data0.values[testidx0, 0:FEATURE_NUM]
+    t0 = data0.values[testidx0, FEATURE_NUM]
+    e0 = data0.values[testidx0, FEATURE_NUM+1]
+
+    x1 = data1.values[testidx1, 0:FEATURE_NUM]
+    t1 = data1.values[testidx1, FEATURE_NUM]
+    e1 = data1.values[testidx1, FEATURE_NUM+1]
+
+    x = np.concatenate((x0, x1), axis=0)
+    t = np.concatenate((t0, t1), axis=0)
+    e = np.concatenate((e0, e1), axis=0)
+    #normlize
+    df = pd.DataFrame(x)
+    df = df.replace(-1, 9)
+    ndf = (df - df.min()) / (df.max() - df.min())
+    x = ndf.values
+
+    test_data = {
+        'x': x.astype(np.float32),
+        't': t.astype(np.int8),
+        'e': e.astype(np.int8)
+    }
+    #test_data['x'] -= np.mean(test_data['x'], axis=0)
+    print 'test t=1: min-', min(t1), ' max-', max(t1), ' mean-', np.mean(t1)
+    return (train_data, test_data, names)
+
+

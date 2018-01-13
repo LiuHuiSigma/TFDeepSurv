427a7a45535650bd1423a92de6d40c0b7e249e06
Pei Liu <18200144374@163.com>
2018-01-13 17:52:56 +0800

    Fixed x-ticks plot.

--- utils_vis.py
+++ utils_vis.py
@@ -1,16 +1,10 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_train_curve(L, title='Training Curve', x_point=5):
+def plot_train_curve(L, title='Training Curve'):
 	x = range(1, len(L) + 1)
-	xticks = []
-	for i in x:
-		if i % (len(L)/x_point) == 0:
-			xticks.append(i)
-		else:
-			xticks.append('')
-	xticks[0] = '1'
 	plt.plot(x, L)
-	plt.xticks(x, xticks)
+	# no ticks
+	plt.xticks([])
 	plt.title(title)
 	plt.show()
